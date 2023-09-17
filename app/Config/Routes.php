12d8f13dcb9b2de3803fd93b5aaa005aa25094f5
Kenzaki1973 <Jitromanalo@gmail.com>
2023-09-18 00:14:21 +0800

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/Kenzaki1973', 'Home::Kenzaki1973');
-
+$routes->get('/product', 'ProductController::Kenzaki1973');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
